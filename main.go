f609af1f99920c28ad7d1619d00ee6ce1e91f27e
Prateek Khened <code.prateekkhened@gmail.com>
2025-03-24 23:25:42 +0530

    code organise

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -34,83 +32,4 @@ func main() {
 	}
 	log.Printf("serving files from %s on port %s\n", filePathRoot, port)
 	log.Fatal(s.ListenAndServe())
-}
-
-func ReadinessHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) CountHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())))
-}
-
-func (cfg *apiConfig) ResetHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	cfg.fileserverHits.Store(0)
-	w.Write([]byte("Reset Successful"))
-}
-
-func ValidateChirpHandler(w http.ResponseWriter, r *http.Request) {
-	type request struct {
-		Body string `json:"body"`
-	}
-	type errorRes struct {
-		Error string `json:"error"`
-	}
-	type chirpRes struct {
-		Valid bool `json:"valid"`
-	}
-	decoder := json.NewDecoder(r.Body)
-	req := request{}
-	err := decoder.Decode(&req)
-	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		resErr := errorRes{
-			Error: "Something went wrong",
-		}
-		dat, err := json.Marshal(resErr)
-		if err != nil {
-			return
-		}
-		w.Write(dat)
-		return
-	}
-
-	if len(req.Body) > 140 {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		resChirp := errorRes{
-			Error: "Chirp is too long",
-		}
-		dat, err := json.Marshal(resChirp)
-		if err != nil {
-			return
-		}
-		w.Write(dat)
-	} else {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		resChirp := chirpRes{
-			Valid: true,
-		}
-		dat, err := json.Marshal(resChirp)
-		if err != nil {
-			return
-		}
-		w.Write(dat)
-	}
-
 }
---
+++ reset.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) ResetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Reset Successful"))
+}
